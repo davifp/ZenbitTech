2d3972e2cb0135353582f516d54e8b371e15ba47
Davi Pavone <davipavone@gmail.com>
2022-10-07 19:31:06 -0300

    feature: add mysql database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
+import { ModuleModule } from './message/module/module.module';
+import { MessageModule } from './modules/message/message.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, ModuleModule, MessageModule],
   controllers: [],
   providers: [],
 })
--- backend/src/messages/dto/create-message.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateMessageDto {
-  name: string;
-  email: string;
-  message: string;
-}
